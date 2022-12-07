50cb3830291d2a0a16e59496e69070632411b6a7
Brandon <1bddouglas@gmail.com>
2022-12-07 13:37:24 -0500

    review display

--- functions/src/routes/reviewRouter.ts
+++ functions/src/routes/reviewRouter.ts
@@ -9,6 +9,22 @@ const errorResponse = (error: any, res: any) => {
   res.status(500).json({ message: "Internal Server Error" });
 };
 
+// display reviews
+reviewRouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<Review>("reviews")
+      .find({ reviewId: id })
+      .toArray();
+    res.status(200).json(result);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
 // create a review
 reviewRouter.post("/", async (req, res) => {
   const newReview: Review = req.body;
