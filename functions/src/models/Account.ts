536588ba71ceba5779fd6925d3f788fbd1c5fe4b
Brandon <1bddouglas@gmail.com>
2022-12-09 15:56:06 -0500

    friend links

--- functions/src/models/Account.ts
+++ functions/src/models/Account.ts
@@ -1,12 +1,18 @@
 import { ObjectId } from "mongodb";
 import BoardGame from "./BoardGame";
 
+interface Friends {
+  name: string;
+  picture: string;
+  id: string;
+}
+
 export default interface Account {
   _id?: ObjectId;
   name: string;
   wishlist: BoardGame[];
   myShelf: BoardGame[];
-  myFriends: string[];
+  myFriends: Friends[];
   uid: string;
   profilePic: string;
 }
